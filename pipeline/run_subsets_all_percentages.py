3ced4b177ce971a8046d7bbfe7c477cf82a2aaf1
Paul A. Bloom <pab2163@columbia.edu>
2020-08-19 02:16:29 -0400

    fixing path bugs

--- pipeline/run_subsets_all_percentages.py
+++ pipeline/run_subsets_all_percentages.py
@@ -5,10 +5,10 @@ import os
 def run_subsets_all_percentages(input_data_path, n_subsets, n_boots, mri, n_cpus, output_dir):
 	# run boostrapping on each subset for each percentage
 	for pct in [10,20,30,40,50,60,70,80,90]:
-		os.system(f'python run_louvain_subsets.py ../data/split1.csv {pct} {n_subsets} {n_boots} {n_cpus} False {output_dir}')
+		os.system(f'python run_louvain_subsets.py {input_data_path} {pct} {n_subsets} {n_boots} {n_cpus} False {output_dir}')
 
 	# Full dataset
-	os.system(f'python run_louvain_subsets.py ../data/split1.csv 100 1 1 1 False {output_dir}')
+	os.system(f'python run_louvain_subsets.py {input_data_path} 100 1 1 1 False {output_dir}')
 
 	# Make the full stability matrix and generate final subtypes for the bagged pipelines for each subset for each subset percentage
 	for i in range(50):
